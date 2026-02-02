9d2b1861c2b925d8af2a06918e73d751b28fb309
Egor <ururugeorge@gmail.com>
2026-02-02 22:11:26 +0300

    chore: added element-plus ui library

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main style='padding: 24px'>
+    <el-button type="primary">Element Plus Btn</el-button>
+  </main>
 </template>
 
 <style scoped></style>
